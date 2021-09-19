b92162d09a999de41e5ec4fcd83f0bf6ab2ccbfb
João Fortunato Jr <joao.fortunatojr@hotmail.com>
2021-09-19 16:14:14 -0300

    NavBar responsiva

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  private mobileMenu: any;
+  private navList: any;
+  private navLinks: any;
+  private activeClass: string;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.mobileMenu = document.querySelector(".mobile-menu");
+    this.navList = document.querySelector(".nav-list");
+    this.navLinks = document.querySelectorAll(".nav-list li");
+    this.activeClass = "active";
+
+    if (this.mobileMenu) {
+      this.mobileMenu.addEventListener("click", () => this.onMenuClick());
+    }
+  }
+
+  onMenuClick(): void {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+
+    this.navLinks.forEach((link, index) => {
+      link.style.animation ? (link.style.animation = "") :
+        (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`);
+    });
+  }
 }
