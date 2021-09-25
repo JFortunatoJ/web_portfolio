4bf5c6bcc961e44ece311864b7e76ee3a9f055ec
João Fortunato Jr <joao.fortunatojr@hotmail.com>
2021-09-25 02:43:09 -0300

    Página temporária dos projetos responsiva

--- src/app/content/main/projects/project-element/project-element.component.ts
+++ src/app/content/main/projects/project-element/project-element.component.ts
@@ -12,9 +12,28 @@ export class ProjectElementComponent implements OnInit {
   @Input()
   public project: Project;
 
+  private projectName: any;
+
   constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+
+    this.projectName = document.querySelectorAll('.project-name label');
+
+    this.projectName.forEach(box => {
+      box.style.fontSize = this.getFontSize(box.textContent.length);
+    });
+
+  }
+
+  getFontSize(textLength): string {
+    const baseSize = 7;
+    const baseTextLength = 13;
+    if (textLength > baseTextLength) {
+      return `${((baseTextLength * baseSize) / textLength) + 1}vw`;
+    } else {
+      return `${baseSize}vw`;
+    }
   }
 }
